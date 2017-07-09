36113deb80d1de744d1dba4954395863f809a285
ruiming <ruiming.zhuang@gmail.com>
2017-07-09 23:28:17 +0800

    feat: update judge

--- src/Judger.ts
+++ src/Judger.ts
@@ -2,8 +2,16 @@
  * 判题 Judger
  */
 import * as buffer from 'buffer'
+import { exec } from 'child_process'
 import * as Docker from 'dockerode'
+import * as fs from 'fs'
+import * as path from 'path'
 import * as Stream from 'stream'
+import * as util from 'util'
+
+const writeFileAsync = util.promisify(fs.writeFile)
+const mkdirAsync = util.promisify(fs.mkdir)
+const repository = path.resolve(__dirname, '..', 'repository')
 
 export interface ILimit {
   maxCpuTime: number
@@ -14,6 +22,7 @@ export interface ILimit {
 }
 
 export interface ISubmission extends ILimit {
+  id: number
   code: string
   inputData: string
 }
@@ -22,27 +31,45 @@ const docker = new Docker({
   socketPath: '/var/run/docker.sock'
 })
 
+/**
+ * TODO 待优化
+ * 1. 重用 py 和输入文件, 确保生成一次
+ * 2. 容器内的判题不要对外部产生任何副作用(不要生成输出文件, 日志文件到外部来)
+ * 3. 优化 py 脚本, 在容器内对比判题输出和期望输出是否一致
+ * 4. 增强容错性
+ */
 export class Judger {
   private docker: Docker
   private container: Docker.Container
-
-  private readonly sourceCodePath: string = '/root/main.cc'
-  private readonly inputDataPath: string = '/root/main.in'
-  private readonly outputDataPath: string = '/root/main.out'
-  private readonly errorPath: string = '/root/error.txt'
-  private readonly logPath: string = '/root/log.txt'
-  private readonly script: string = '/root/main.py'
+  private readonly timestamp: string
+  private readonly outerWorkDir: string
+  private readonly innerWorkDir: string
+  private readonly execFilePath: string
+  private readonly execFileInputPath: string
+  private readonly outputDataPath: string
+  private readonly errorPath: string
+  private readonly logPath: string
+  private readonly script: string = '/root/main.py' // 存 docker 即可
   private stream: Stream
   private submission: ISubmission
 
   constructor (submission: ISubmission) {
     this.docker = docker
     this.submission = submission
+    this.timestamp = Date.now().toString()
+    this.outerWorkDir = path.resolve(__dirname, '..', 'repository', this.timestamp)
+    this.innerWorkDir = path.join('/root', this.timestamp)
+    this.execFilePath = path.join(this.innerWorkDir, this.submission.id.toString())
+    this.execFileInputPath = path.join(this.innerWorkDir, `${this.submission.id}.in`)
+    this.outputDataPath = path.join(this.innerWorkDir, 'main.out')
+    this.errorPath = path.join(this.innerWorkDir, 'error.txt')
+    this.logPath = path.join(this.innerWorkDir, 'log.txt')
   }
 
   public async process () : Promise<string> {
     let result: string
     try {
+      await mkdirAsync(this.outerWorkDir)
       result = await this.runCode()
     } catch (e) {
       console.log(e)
@@ -63,14 +90,18 @@ export class Judger {
       Tty: true,
       Cmd: ['/bin/bash'],
       OpenStdin: true,
-      StdinOnce: false
+      StdinOnce: false,
+      HostConfig: {
+        Binds: [`${this.outerWorkDir}:${this.innerWorkDir}`]
+      }
     })
-    await this.container.start()
-    const commands: string[] = ['bash', '-c',
-      [this.writeCode(this.submission.code),
-      this.writeInputData(this.submission.inputData),
-      this.judging(this.submission)].join(' && ')
-    ]
+    await Promise.all([
+      this.container.start(),
+      this.writeCode(),
+      this.writeInputData()
+    ])
+    await this.compile()
+    const commands: string[] = ['bash', '-c', this.judging(this.submission)]
     const exec: Docker.Exec = await this.container.exec({
       Cmd: commands,
       AttachStdin: true,
@@ -94,6 +125,9 @@ export class Judger {
     return result
   }
 
+  /**
+   * 销毁容器
+   */
   private async destroyContainer (): Promise<void> {
     await this.container.stop()
     console.log('Container stop success!')
@@ -101,16 +135,43 @@ export class Judger {
     console.log('Container destroy success!')
   }
 
-  private writeCode (sourceCode: string): string {
-    return `echo "${sourceCode}" > ${this.sourceCodePath}`
+  /**
+   * 将用户代码写入文件
+   */
+  private async writeCode (): Promise<void> {
+    await writeFileAsync(path.resolve(this.outerWorkDir, `${this.submission.id}.cc`), this.submission.code)
   }
 
-  private writeInputData (inputData: string): string {
-    return `echo "${inputData}" > ${this.inputDataPath}`
+  /**
+   * 将用户代码的测试输入写入到文件
+   */
+  private async writeInputData (): Promise<void> {
+    await writeFileAsync(path.resolve(this.outerWorkDir, `${this.submission.id}.in`), this.submission.inputData)
   }
 
+  /**
+   * 本机编译用户的代码
+   */
+  private compile (): Promise<{}> {
+    return new Promise((resolve, reject) => {
+      exec(`/usr/bin/g++ ${path.resolve(this.outerWorkDir, `${this.submission.id}.cc`)} \
+            -o ${path.resolve(this.outerWorkDir, `${this.submission.id}`)} \
+            -w -lm --static -DONLINE_JUDGE`, (err, stdout, stderr) => {
+        if (err || stderr) {
+          reject(err || stderr)
+        }
+        resolve()
+      })
+    })
+  }
+
+  /**
+   * 返回一段运行脚本
+   * @param limit 资源限制
+   */
   private judging (limit: ILimit): string {
-    return `echo "${this.wrapData(limit)}" > ${this.script} && /usr/bin/python3 ${this.script}`
+    return `echo "${this.wrapData(limit)}" > ${this.script} &&
+            /usr/bin/python3 ${this.script}`
   }
 
   private wrapData (limit: ILimit): string {
@@ -121,8 +182,8 @@ export class Judger {
       max_process_number=${limit.maxProcessNumber},
       max_output_size=${limit.maxOutputSize},
       max_stack=33554432,
-      exe_path='${this.sourceCodePath}',
-      input_path='${this.inputDataPath}',
+      exe_path='${this.execFilePath}',
+      input_path='${this.execFileInputPath}',
       output_path='${this.outputDataPath}',
       error_path='${this.errorPath}',
       args=[],
--- src/index.ts
+++ src/index.ts
@@ -29,15 +29,31 @@ new Promise((resolve: () => void, reject: (e: Error) => void) => {
   for ( ; ; ) {
     const message = await comsumer.brpop(['JUDGER'], 0)
     const submissionId = message[1]
-    const submission = await getSubmissionById(submissionId)
-    if (submission) {
-      const judger = new Judger(submission)
-      const result = await judger.process()
+    console.log(submissionId)
+    getSubmissionById(submissionId).then(async submission => {
+      if (submission) {
+        const judger = new Judger(submission)
+        const result = await judger.process()
+        console.log(result)
+        await producer.lpush('JUDGER_FINISH', JSON.stringify({
+          ...JSON.parse(result.replace(/'/g, '"')),
+          submissionId
+        }))
+      }
+    }).catch(async e => {
+      console.log('编译出错', e)
       await producer.lpush('JUDGER_FINISH', JSON.stringify({
-        ...JSON.parse(result.replace(/'/g, '"')),
-        submissionId
+        submissionId,
+        exit_code: -1,
+        memory: -1,
+        result: 6,  // 编译错误
+        cpu_time: -1,
+        signal: -1,
+        real_time: -1,
+        error: -1
       }))
-    }
+    })
+
   }
 }).catch((e: Error) => console.log(e))
 
@@ -47,7 +63,7 @@ const mock = async (i: number) => {
 
 const getSubmissionById = (submissionId: number): Promise<ISubmission> => {
   return new Promise((resolve: (res: ISubmission) => void, reject: (err: Error) => void) => {
-    db.query(`SELECT * FROM Submission INNER JOIN Problem ON Submission.problemId = Problem.id WHERE Submission.id = ${submissionId}`,
+    db.query(`SELECT * FROM submission INNER JOIN problem ON submission.problemId = problem.id WHERE submission.id = ${submissionId}`,
       (err: Error, results: ISubmission[]) => {
         if (err) {
           reject(err)
